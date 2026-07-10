2f75023a7e38d8d21b536f2374cf01c8aa6722b0
Atharva Chandurkar <atharvachandurkar7115@gmail.com>
2026-07-10 11:48:14 +0530

    project structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>JalDrishti Groundwater Monitoring Platform</h1>
+    </div>
+  );
+}
+
+export default App;
