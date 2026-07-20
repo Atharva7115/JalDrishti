baff99f9e9395988d6da4701a257654b90a492d0
Atharva Chandurkar <atharvachandurkar7115@gmail.com>
2026-07-20 20:22:40 +0530

    added ingestion pipeline

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,9 +2,16 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import app from "./app.js";
+import { ingestGroundwaterData } from "./services/ingestion.service.js";
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`🚀 Server running on port ${PORT}`);
+
+  try {
+    await ingestGroundwaterData();
+  } catch (error) {
+    console.error("Error during ingestion:", error.message);
+  }
 });
